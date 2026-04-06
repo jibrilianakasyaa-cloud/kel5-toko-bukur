2e16606778d6e4035030636d133b5b161df6150d
Jibril <jibrilianakasyaa@gmail.com>
2026-04-06 11:28:51 +0700

    revamp

--- js/script.js
+++ js/script.js
@@ -1,11 +1,1 @@
-const navItems = document.querySelectorAll('.nav-item');
 
-navItems.forEach(item => {
-  item.addEventListener('click', () => {
-    document.querySelector('.active').classList.remove('active');
-    item.classList.add('active');
-  });
-});
-function goToPage() {
-  window.location.href = "all.html"; // page tujuan
-}
